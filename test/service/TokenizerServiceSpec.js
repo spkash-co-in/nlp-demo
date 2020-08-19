f9c82f6983e1d9c08056eb9b6e41f82b7147ec27
spkash-co-in <spkash.co.in@gmail.com>
2020-08-19 13:52:29 +0530

    cleanup

--- test/service/TokenizerServiceSpec.js
+++ test/service/TokenizerServiceSpec.js
@@ -4,18 +4,9 @@ require('../testCommon');
 const testPayload = require('../test-data/payload');
 const TokenizerService = require('../../service/tokenizerService');
 const Constants = require('../../common/constants');
-const StemService = require('../../service/stemService');
-const Question = require('../../model/Question');
-const Answer = require('../../model/Answer');
-const AnswerSenteceMapperService = require('../../service/answerSentenceMapperService');
-const QuestionAnswerMapperService = require('../../service/QuestionAnswerMapperService');
-
 
 describe('Given a tokenizer service', () => {
     let tokenizerService = new TokenizerService();
-    let stemService = new StemService();
-    let answerSenteceMapperService = new AnswerSenteceMapperService();
-    let questionAnswerMapperService = new QuestionAnswerMapperService();
     it('should remove new lines', () => {
         const inputWithoutNewLines = tokenizerService.removeNewLines(testPayload.paragraphSection);
         expect(inputWithoutNewLines.includes("\n")).to.equal(false);
